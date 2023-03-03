6c48e9c2913220f5e5c68a03246f67dc8ac10b34
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-03-03 01:07:34 +0100

    Added info on the go.write-ip-to-remote.sh script and improved messages

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Simple utilities (mainly bash) to work with IPs
 
 This was creates within the EYEFUL project, and the first tool was one to allow a remote machine to remotely write a file in a given server (with known IP), that includes the remote IP. This is a silly non-generic bad approach that want to remotely look like DynDNS (ha!)
+
+# Available scripts
+
+`go.write-ip-to-remote.sh`: That reads the IP of the active network connection, and writes it to a file in a remote server using ssh. It requires the IP of the remote server, the username, the destination path in the remote server, and also that the ssh connection do not require a password (it's in the authorized_keys file) if you don't want it to prompt for one
--- go.write-ip-to-remote.sh
+++ go.write-ip-to-remote.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+HOSTNAME=`hostname`
+
 # Set the remote server details
-REMOTE_SERVER="example.com"
+REMOTE_SERVER="127.0.0.1"
 REMOTE_USERNAME="macias"
-REMOTE_PATH="/path/to/remote/file.txt"
+REMOTE_PATH="/home/macias/ip-$HOSTNAME.txt"
 
 
 # THis solution is bad as it may return several IPs
 # Get the IP address of the local machine
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
+# IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 # Get the IP address of the active network connection
 IP_ADDRESS=$(ip route get 1 | awk '{print $7}')
 
 # Write the IP address to the remote file
+echo "Running <ssh $REMOTE_USERNAME@$REMOTE_SERVER \"echo $IP_ADDRESS >> $REMOTE_PATH\">"
 ssh $REMOTE_USERNAME@$REMOTE_SERVER "echo $IP_ADDRESS >> $REMOTE_PATH"
